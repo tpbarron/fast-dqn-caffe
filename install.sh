3e4998ec21d0155528973d26f72fefe55ff081f5
Trevor Barron <barron.trevor@gmail.com>
2016-01-06 13:05:49 -0700

    adding dependencies to install script

--- install.sh
+++ install.sh
@@ -4,7 +4,12 @@ rm -rf dependencies/
 mkdir -p dependencies/caffe/
 
 # download caffe distribution 
-echo "Downloading compatible CAFFE distribution"
+echo "Installing dependencies for Caffe"
+sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler
+sudo apt-get install --no-install-recommends libboost-all-dev
+sudo apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev
+
+echo "Downloading compatible Caffe distribution"
 wget http://tpbarron.github.io/sources/distribute_ff16f6e43dd718921e5203f640dd57c68f01cdb3.tar.gz --directory-prefix=dependencies/caffe
 cd dependencies/caffe/
 tar xfz distribute_ff16f6e43dd718921e5203f640dd57c68f01cdb3.tar.gz
@@ -12,6 +17,8 @@ gunzip -r distribute_ff16f6e43dd718921e5203f640dd57c68f01cdb3
 cd ../../
 
 # download and make minecraft interface
+echo "Installing dependencing for ALE"
+sudo apt-get install libsdl1.2-dev
 echo "Downloading and building ALE"
 git clone https://github.com/mgbellemare/Arcade-Learning-Environment.git dependencies/Arcade-Learning-Environment
 cd dependencies/Arcade-Learning-Environment/
