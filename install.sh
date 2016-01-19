56eb34def2811dbb005067c4fa8f111246265ccd
Trevor Barron <barron.trevor@gmail.com>
2016-01-18 18:53:31 -0700

    adding PyCaffe dependencies

--- install.sh
+++ install.sh
@@ -8,7 +8,11 @@ echo "Installing dependencies for Caffe"
 sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler
 sudo apt-get install --no-install-recommends libboost-all-dev
 sudo apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev
- 
+
+echo "Installing PyCaffe dependencies"
+sudo apt-get install python-skimage
+sudo apt-get install python-protobuf
+
 echo "Downloading compatible CAFFE distribution"
 wget http://tpbarron.github.io/sources/distribute_ff16f6e43dd718921e5203f640dd57c68f01cdb3.tar.gz --directory-prefix=dependencies/caffe
 cd dependencies/caffe/
